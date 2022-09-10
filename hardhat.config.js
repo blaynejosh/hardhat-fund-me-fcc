fb59b12aa2eb1448366e012fdffd20fa518d717e
blaynejosh <jesulayomio@gmail.com>
2022-09-10 16:52:05 +0100

    network updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -14,11 +14,11 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const KOVAN_RPC_URL =
     process.env.KOVAN_RPC_URL ||
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const RINKEBY_RPC_URL =
-    process.env.RINKEBY_RPC_URL ||
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL ||
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
 const PRIVATE_KEY =
-    process.env.PRIVATE_KEY ||
+    process.env.GOERLI_PRIVATE_KEY ||
     "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 
